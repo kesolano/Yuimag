5ac3cc84d023fdc908787f4100dcf11a4c920ed7
Kevin Solano <ke.solano@uniandes.edu.co>
2023-04-30 20:16:59 -0500

    Change statis path

--- Yuimag/urls.py
+++ Yuimag/urls.py
@@ -17,10 +17,15 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
+from django.conf.urls import url
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("app.urls")),
     path('accounts/', include('django.contrib.auth.urls')),
+    url(r'^static/("https://yuimag.s3.us-east-2.amazonaws.com/".*)$', serve,
+        {'document_root': settings.STATIC_ROOT}),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
